46f03a7e2797e752d0ee6e6f1ae889636fc5fda5
Rae Bonfanti <itnafnobear@gmail.com>
2014-07-20 18:04:12 -0700

    css changes and fewer instagrams

--- javascripts/instagram_feed.js
+++ javascripts/instagram_feed.js
@@ -1,12 +1,17 @@
-function imageTemplate(url) {
-  return "<img src='" + url + "'>";
+function makeAltText(caption) {
+  return !!caption ? caption.text : '';
+}
+
+function imageTemplate(source, alt) {
+  return "<img src='" + source + "' alt='" + alt + "'>";
 }
 
 function parseImageData(data) {
   var imageTags = [];
   for(var i = 0, image; image = data[i]; i++) {
-    var url = image.images.standard_resolution.url;
-    var template = imageTemplate(url);
+    var source = image.images.standard_resolution.url;
+    var alt = makeAltText(image.caption);
+    var template = imageTemplate(source, alt);
     imageTags.push(template);
   }
   return imageTags.join("");
@@ -15,7 +20,7 @@ function parseImageData(data) {
 $.ajax({
   type: "GET",
   "method": "GET",
-  url: 'https://api.instagram.com/v1/users/38878930/media/recent?client_id=e5ecc8cb303e44e2bbc7a1a43eeea118',
+  url: 'https://api.instagram.com/v1/users/38878930/media/recent?count=5&client_id=e5ecc8cb303e44e2bbc7a1a43eeea118',
   dataType: "jsonp",
   success: function(response){
     var data = response.data;
